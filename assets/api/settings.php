bd525236b809e70fa7d67124bf694d0fd17d7de4
Nevercold <moritz.walter111@gmx.de>
2019-08-11 15:16:07 +0200

    add function getsetting

--- assets/api/settings.php
+++ assets/api/settings.php
@@ -9,5 +9,22 @@
 function setsetting(PDO $pdo, $settingkey, $settingvalue){
     $statement = $pdo->prepare("UPDATE setting SET value = ? WHERE key = ?");
     $statement->execute(array($settingvalue, $settingkey));
+    return true;
+}
+
+function getsetting(PDO $pdo, $settingkey){
+    $statement = $pdo->prepare("SELECT ".$settingkey." FROM setting WHERE key = ? LIMIT 1");
+    $statement->execute(array($settingkey));
+    if($statement->execute()) {
+        if ($statement->rowCount() == 1) {
+            while ($row = $statement->fetch()) {
+                return $row['value'];
+            }
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 }
 
