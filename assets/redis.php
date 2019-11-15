4788273ddeff8217a922b5eed8e4c64a96396587
Niekold <moritz.walter111@gmx.de>
2019-11-15 18:30:23 +0100

    redis

--- assets/redis.php
+++ assets/redis.php
@@ -6,4 +6,19 @@
  * File created on 13.11.2019 at 17:14
  */
 
-ll;
+
+// mysql login data
+define('redis-ip', '');
+define('redis-port', '');
+define('redis-pw', '');
+
+$redisenabled = false;
+if($redisenabled == true){
+    Predis\Autoloader::register();
+    $redis = new Predis\Client(array(
+        "scheme" => "tcp",
+        "host" => redis-ip,
+        "port" => redis-port,
+        "password" => redis-pw
+    ));
+}
